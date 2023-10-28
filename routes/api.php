3af8ac1d7403a6d5c5238a6295312a7da9a3c9af
noor <noor.tiaima@gmail.com>
2023-10-29 01:48:00 +0200

    login api

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
 use App\Http\Controllers\api\LawyerController;
 use App\Http\Controllers\api\SpecializationController;
 
@@ -19,11 +24,39 @@ use App\Http\Controllers\api\CityController;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-
+/*
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+*/
 
+Route::post('/sanctum/token', function (Request $request) {
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+        'device_name' => 'nullable',
+    ]);
+ 
+    $user = User::where('email', $request->email)->first();
+    //if (! $user || ! $request->password == $user->password) {
+    if (! $user || ! Hash::check($request->password, $user->password)) {
+        throw ValidationException::withMessages([
+            'email' => ['The provided credentials are incorrect.'],
+        ]);
+    }
+ 
+    return $user->createToken($request->device_name)->plainTextToken;
+});
+
+Route::post('/logout', function (Request $request) {
+
+    $user = Auth::guard('sanctum')->user();
+    $token =  $user->currentAccessToken();
+    $token->delete();
+
+    return response('Logged_out', 200);
+
+});
 Route::apiResource('lawyers',LawyerController::class);
 
 Route::post('lawyers/search',[LawyerController::class,'search']);
