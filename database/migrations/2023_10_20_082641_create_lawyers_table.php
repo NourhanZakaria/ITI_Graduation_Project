ec280f6a06e6afe533bf5d394ad085bfe157b903
NourhanZakaria <NourhanZakaria43@gmail.com>
2023-11-03 18:44:06 +0300

    verified

--- database/migrations/2023_10_20_082641_create_lawyers_table.php
+++ database/migrations/2023_10_20_082641_create_lawyers_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->integer('span');
             $table->string('location');
             $table->string('about');
-            $table->boolean('verified')->default('false');
+            $table->boolean('verified')->default(false);
             $table->timestamps();
             $table->foreignId('user_id')->references('id')->on('users')->onUpdate("cascade")->onDelete("cascade");
             $table->string('idImage');
