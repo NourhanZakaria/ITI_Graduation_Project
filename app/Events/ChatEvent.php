3dc94b28ff42bdc1ec0b57918a843ac1c400b49f
“mostafa-amr” <mamr1527@gmail.com>
2023-11-15 13:48:52 +0200

    sada

--- app/Events/ChatEvent.php
+++ app/Events/ChatEvent.php
@@ -32,7 +32,7 @@ class ChatEvent implements ShouldBroadcast
     public function broadcastOn(): array
     {
         return [
-            new Channel('chat' . $this->chat->receiver_id),
+            new Channel('chat'),
         ];
     }
     public function broadcastAs()
